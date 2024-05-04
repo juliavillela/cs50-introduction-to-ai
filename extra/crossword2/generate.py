87db1d1cd2d889519d46cf6cf263f24ed953410d
juliavillela <julia.arruda@gmail.com>
2024-05-04 15:31:02 -0300

    refactor crosswordBuilder

--- extra/crossword2/generate.py
+++ extra/crossword2/generate.py
@@ -3,89 +3,128 @@ from random import shuffle
 from constants import *
 from crossword import CrosswordGrid
 
+MAX_WORDS = 25
+MIN_WORDS = 3
+MAX_WORD_LEN = 35
+MIN_WORD_LEN = 3
+
 class CrosswordBuilder:
-    def __init__(self, words:list, grid_size:int) -> None:
-        words.sort(key= lambda w: len(w), reverse=True )
-        self.words = words
+    def __init__(self, words:list, grid_size, max_iteration_count=50) -> None:
+        validate_word_list(words)
+        self.words = sorted(words, key=lambda w: len(w), reverse=True)
         self.grid_size = grid_size
-        self.grid = None
+        self.max_iteration_count = max_iteration_count
 
     def __str__(self):
         return f"crossword generator({self.grid_size}): {self.words}"
     
-    def try_to_place(self, word, overlap_col, overlap_row, overlap_index):
-        (row, col) = get_absolute_placement(overlap_col, overlap_row, overlap_index, HORIZONTAL)
-        if self.grid.can_place_horizontal(word, row, col):
-            self.grid.place_word(word, row, col, HORIZONTAL)
-            return True
-        else:
-            # try vertical placement
-            (row, col) = get_absolute_placement(overlap_col, overlap_row, overlap_index, VERTICAL)
-            if self.grid.can_place_vertical(word, row, col):
-                self.grid.place_word(word, row, col, VERTICAL)
-                return True
-        return False
+    def get_many_valid_word_placements(self, word, grid):
+        """
+        Returns a list of tuples ((row,col), direction) for each 
+        valid placement of word in grid
+        """
+        valid_placements = []
+        # iterate through each char in word to find matching chars in the grid
+        for index, char in enumerate(word):
+            matches = grid.match_many_char(char)
+            
+            for match in matches:
+                h_col = match[1] - index
+                h_row = match[0] 
+                h_can_place = grid.can_place_horizontal(word, h_row, h_col)
+                if h_can_place:
+                    valid_placements.append(((h_row, h_col), HORIZONTAL)) 
+                
+                v_col = match[1]
+                v_row = match[0] - index
+                v_can_place = grid.can_place_vertical(word, v_row, v_col)
+                if v_can_place:
+                    valid_placements.append(((v_row, v_col), VERTICAL))
+        return valid_placements
     
     def iterative_placement(self):
-        queue = self.words.copy()
-        self.grid = CrosswordGrid(self.grid_size)
-        # place first word at the center of the grid
-        first_word = queue.pop(0)
-        (row,col) = self.grid.get_center_placement(first_word, HORIZONTAL)
-        self.grid.place_word(first_word, row, col, HORIZONTAL)
+        # create a blank grid
+        grid = CrosswordGrid(self.grid_size)
+        words = self.words.copy()
         
-        shuffle(queue)
-        # limit iteration count to twice the len of words
-        max_iterations = 2 * len(queue)
+        # place first word
+        words.sort(key= lambda x: len(x), reverse=True)
+        first_word = words.pop(0)
+        (row, col) = grid.get_center_placement(first_word, HORIZONTAL)
+        grid.place_word(first_word, row, col, HORIZONTAL)
+        
+        # shuffle words to get random placement order
+        shuffle(words)
+        max_iterations = 2 * len(words)
         iteration_count = 0
-
-        while iteration_count < max_iterations and len(queue) != 0:
+        
+        while len(words) and iteration_count<max_iterations:
+            word = words.pop(0)
             iteration_count += 1
-            placed = False
-            word = queue.pop(0)
-            for index, char in enumerate(word):
-                match = self.grid.find_char(char)
-                if match:
-                    placed = self.try_to_place(word, match[0], match[1], index)
-                    if placed:
-                        break
-            if not placed:
-                queue.append(word)
-        if len(queue):
-            return False
+            # valid_placement = self.get_valid_word_placement(word, grid)
+
+            # get all valid placements for word in current grid
+            valid_placements = self.get_many_valid_word_placements(word, grid)
+            
+            # shuffle to select placement at random
+            shuffle(valid_placements)
+
+            # if valid_placement:
+            if len(valid_placements):
+                valid_placement = valid_placements[0]
+                ((row,col), direction) = valid_placement
+                grid.place_word(word, row, col, direction)
+            else:
+                words.append(word)
+        # return grid (which might be incomplete)
+        return grid
+
+    def generate(self):
+        """
+        Return grid with self.words calling iterative_placement multiple times
+        untill all words have been placed in the grid or until self.max_iteration_count
+        
+        If could not place all words in grid, returns None
+        """
+        # starting with grid-size
+        # iteratively place words x timess
+        grid = self.iterative_placement()
+        iteration_counter = 0
+        # stop when all words have been placed in grid or when iteration reaches max_count
+        while len(self.words) != len(grid.words) and iteration_counter < self.max_iteration_count:
+            iteration_counter += 1
+            grid = self.iterative_placement()
+        
+        print("iteration count", iteration_counter)
+        if len(self.words) != len(grid.words):
+            return None
         else:
-            self.grid.trim()
-            return self.grid
+            return grid
 
-    def save(self, filename):
-        self.grid.trim()
-        text_rows = []
-        for row in self.grid.grid:
-            text_rows.append([char or " " for char in row])
-
-        with open(f"./{filename}", "w") as txt_file:
-            txt_file.writelines([" ".join(row)+"\n" for row in text_rows])
-
-def get_absolute_placement(overlap_col, overlap_row, overlap_index, direction):
-    if direction == VERTICAL:
-        return (overlap_col - overlap_index, overlap_row)
-    else:
-        return (overlap_col, overlap_row-overlap_index)
-
-
-
-def validate_wordlist(word_list):
+def validate_word_list(word_list):
     """
-    Return True if word_list is valid, else false.
-    A word_list is valid if 
-    - it has more than MIN_WORDS and less than MAX_WORDS
+    Raises an error if word list does not meet any of the criteria
+
+    A word_list is valid if:
+    - it has more than MIN_WORDS and less than MAX_WORDS.
+    - no words in list are either too long or too short
     - each word has at least one char in common with another word.
     """
-    # length is within range
-    if len(word_list) < MIN_WORDS or len(word_list) > MAX_WORDS:
-        return False
+    # word list is of adequate length
+    if len(word_list) > MAX_WORDS or len(word_list) < MIN_WORDS:
+        raise ValueError(f"word list should have between {MIN_WORDS} and {MAX_WORDS}, not {len(word_list)}")
+
+    # no words are too long
+    too_long = [w for w in word_list if len(w)>MAX_WORD_LEN]
+    if len(too_long):
+        raise ValueError(f"word list contains words that are longer than {MAX_WORD_LEN}: {too_long}")
     
-    # words should have at least one char in common with another word
+    # no words are too short
+    too_short = [w for w in word_list if len(w)<MIN_WORD_LEN]
+    if len(too_short):
+        raise ValueError(f"word list contains words that are shorter than {MIN_WORD_LEN}: {too_short}")
+    
+    # words should have at least one char in common with another word        
     for i, word1 in enumerate(word_list):
         found_common = False
         for j, word2 in enumerate(word_list):
@@ -94,8 +133,8 @@ def validate_wordlist(word_list):
                     found_common = True
                     break
         if not found_common:
-            print(f"word '{word1}' has no characters in common with other words")
-            return False
+            raise ValueError(f"word list is impossible. word '{word1}' has no chars in common with other words")
+    
     return True
 
 class CrosswordGenerator:
@@ -110,7 +149,7 @@ class CrosswordGenerator:
         for a failure in generating a valid grid. 
         """
         
-        if not validate_wordlist(words):
+        if not validate_word_list(words):
             raise ValueError("word list is not valid")
         
         words.sort(key=lambda w: len(w), reverse=True)
